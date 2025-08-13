e448d85fea7492dd7c465f144f0de545366845df
zaselalk <43272744+zaselalk@users.noreply.github.com>
2025-08-13 16:19:48 +0530

    :recycle: Update admin sidebar labels and permissions, refactor admin routes, and enhance user details display

--- src/components/routes/AdminRoutes.tsx
+++ src/components/routes/AdminRoutes.tsx
@@ -9,8 +9,6 @@ import { useAppSelector } from "../../hooks/state/hooks";
 import AnalyticsDashboard from "../../pages/admin/AnalyticalDashbordPage";
 import OfficerDashboard from "../../pages/admin/Officerdashbord";
 
-
-
 export const AdminRoutes = () => {
   const user = useAppSelector((state) => state.auth.user);
 
@@ -33,7 +31,7 @@ export const AdminRoutes = () => {
           path="admin/analytics"
           element={
             // <ProtectedRoutesGuard>
-              <AnalyticsDashboard />
+            <AnalyticsDashboard />
             // </ProtectedRoutesGuard>
           }
         />
@@ -42,7 +40,7 @@ export const AdminRoutes = () => {
           path="admin/officerdashboard"
           element={
             // <ProtectedRoutesGuard>
-              <OfficerDashboard />
+            <OfficerDashboard />
             // </ProtectedRoutesGuard>
           }
         />
@@ -57,23 +55,18 @@ export const AdminRoutes = () => {
           }
         />
 
-
         {/* /admin/users routs */}
         {/* // if the user is super_admin */}
-        {user?.role === "super_admin" && (
-          <Route path="admin/users">
-            <Route
-              path=""
-              element={
-                <ProtectedRoutesGuard>
-                  <UsersPage />
-                </ProtectedRoutesGuard>
-              }
-            />
-          </Route>
-        )}
-
-
+        <Route path="admin/users">
+          <Route
+            path=""
+            element={
+              <ProtectedRoutesGuard>
+                <UsersPage />
+              </ProtectedRoutesGuard>
+            }
+          />
+        </Route>
       </Routes>
     </AuthProvider>
   );
