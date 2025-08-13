45aea740ff9d6f3c5c366a09a32653add8f401c5
Ashfa Nisthar <120776447+Ashf123@users.noreply.github.com>
2025-08-13 10:18:02 +0530

    names changed to citizen

--- src/components/routes/CitizenRoutes.tsx
+++ src/components/routes/CitizenRoutes.tsx
@@ -1,10 +1,10 @@
 import { Route, Routes } from "react-router";
 import { ResidentAuthProvider } from "../auth/ResidentAuthContext";
 import ResidentProtectedRoute from "../auth/ResidentProtectedRoute";
-import ResidentDashboard from "../../pages/resident/ResidentDashboard";
-import ResidentLoginPage from "../../pages/resident/ResidentLoginPage";
-import ResidentRegisterPage from "../../pages/resident/ResidentRegisterPage";
-import CitizenDashboard from "../../pages/resident/CitizenDashboard";
+import ResidentDashboard from "../../pages/citizen/ResidentDashboard";
+import ResidentLoginPage from "../../pages/citizen/CitizenLoginPage";
+import ResidentRegisterPage from "../../pages/citizen/CitizenRegisterPage";
+import CitizenDashboard from "../../pages/citizen/CitizenDashboard";
 
 export const ResidentRoutes = () => {
   return (
