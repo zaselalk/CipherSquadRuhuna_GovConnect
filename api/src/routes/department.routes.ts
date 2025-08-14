90d956c7e51b5dcd582b01ec806f1e3fdb139f95
Dilukshi Nimasha <63289118+dilukshi2001@users.noreply.github.com>
2025-08-14 18:30:56 +0530

    :safety_vest: department backend validation (#96)
    * :sparkles: Add departments migration and seed data for department information
    
    * :sparkles: Implement department management with CRUD operations and routing
    
    * :sparkles: Add validation for department creation, updating, and retrieval
    
    ---------
    
    Co-authored-by: Asela Priyadarshana <43272744+zaselalk@users.noreply.github.com>

--- api/src/routes/department.routes.ts
+++ api/src/routes/department.routes.ts
@@ -1,14 +1,16 @@
 import { Router } from "express";
 import { DepartmentController } from "../controllers/DepartmentController";
 import catchAsync from "../util/catchAsync";
+import { departmentCreateValidation, departmentGetValidation, departmentUpdateValidation } from "../validation/department";
+
 
 const router = Router();
 const controller = new DepartmentController();
 
-router.get("/", catchAsync(controller.getAllDepartments));
-router.get("/:id", catchAsync(controller.getDepartmentById));
-router.post("/", catchAsync(controller.createDepartment));
-router.put("/:id", catchAsync(controller.updateDepartment));
+router.get("/",departmentGetValidation, catchAsync(controller.getAllDepartments));
+router.get("/:id",departmentGetValidation, catchAsync(controller.getDepartmentById));
+router.post("/",departmentCreateValidation, catchAsync(controller.createDepartment));
+router.put("/:id",departmentUpdateValidation, catchAsync(controller.updateDepartment));
 router.delete("/:id", catchAsync(controller.deleteDepartment));
 
 export default router;
