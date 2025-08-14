361d70b9d0fc7b807a21b67c29f80ea377a5ae2f
zaselalk <43272744+zaselalk@users.noreply.github.com>
2025-08-14 15:04:54 +0530

    :bug: Refactor citizen model and repository to enforce required fields and hash passwords before creation

--- api/src/migrations/20250813134928-citizan-model-created.ts
+++ api/src/migrations/20250813134928-citizan-model-created.ts
@@ -26,7 +26,7 @@ module.exports = {
           allowNull: false,
           unique: true,
         },
-        hashPassword: {
+        password: {
           type: DataTypes.STRING,
           allowNull: false,
         },
