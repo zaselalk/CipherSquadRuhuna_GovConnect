d8dce575ba7b40d1082584dea7d0c91bb78f2ed7
RavinduHarshana <ravinduharshana2001@gmail.com>
2025-08-14 15:20:54 +0530

    :sparkles: Add CitizenData interface to define citizen properties

---
+++ src/types/citizen.ts
@@ -0,0 +1,12 @@
+export interface CitizenData {
+    fullName: string;
+    NICNumber: string;
+    email: string;
+    hashPassword: string;
+    confirmPassword: string;
+    birthday: string;
+    gender: string;
+    address: string;
+    contactNumber: string;
+}
+
