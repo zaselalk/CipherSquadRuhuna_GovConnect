de5f0c437849e660f69546c8cb865808ccb7221d
RavinduHarshana <ravinduharshana2001@gmail.com>
2025-08-15 07:37:25 +0530

    :sparkles: refine department_services migration with updated field types and constraints

--- api/src/migrations/20250814132232-service-model.ts
+++ api/src/migrations/20250814132232-service-model.ts
@@ -6,59 +6,55 @@ import { DataTypes, QueryInterface, Sequelize } from "sequelize";
 module.exports = {
   async up(queryInterface: QueryInterface) {
     try {
-      await queryInterface.createTable("department_services", {
-        service_id: {
-          type: DataTypes.INTEGER,
-          primaryKey: true,
-          autoIncrement: true,
-          allowNull: false,
+           await queryInterface.createTable("department_services", {
+      service_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      dep_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "departments", // table name
+          key: "dep_id",
         },
-        name: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          unique: true,
-        },
-        description: {
-          type: DataTypes.STRING,
-          allowNull: true,
-        },
-        department_id: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          references: {
-            model: "departments",
-            key: "dep_id",
-          },
-          onUpdate: "CASCADE",
-          onDelete: "SET NULL",
-        },
-        createdAt: {
-          type: DataTypes.DATE,
-          allowNull: false,
-          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-        },
-        updatedAt: {
-          type: DataTypes.DATE,
-          allowNull: false,
-          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-        },
-        deletedAt: {
-          type: DataTypes.DATE,
-          allowNull: true,
-        },
-      });
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+      deletedAt: {
+        type: DataTypes.DATE,
+        allowNull: true,
+        defaultValue: null,
+      },
+    });
     } catch (error) {
       console.error(error);
       throw new Error("Error creating service_models table: " + error);
     }
   },
 
-  async down(queryInterface: QueryInterface) {
-    try {
-      await queryInterface.dropTable("department_services");
-    } catch (error) {
-      console.error(error);
-      throw new Error("Error dropping service_models table: " + error);
-    }
-  },
+async down(queryInterface: QueryInterface): Promise<void> {
+  await queryInterface.dropTable("department_services");
+},
 };
+
