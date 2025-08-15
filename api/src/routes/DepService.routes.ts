986f6610e3d9ac657f54e5ebcb7b1e8490ed6ffe
RavinduHarshana <ravinduharshana2001@gmail.com>
2025-08-15 17:29:07 +0530

    :sparkles: add DepartmentServiceRouter for handling CRUD operations of department services

---
+++ api/src/routes/DepService.routes.ts
@@ -0,0 +1,15 @@
+// routes/DepartmentServiceRoutes.ts
+import { Router } from "express";
+import { DepartmentServiceController } from "../controllers/DepartmentServiceController";
+import catchAsync from "../util/catchAsync";
+import { DepartmentServiceService } from "../services/DerpartmentServiceService";
+
+const DepartmentServiceRouter = Router();
+const departmentServiceController = new DepartmentServiceController();
+
+DepartmentServiceRouter.post("/", catchAsync(departmentServiceController.createService));
+DepartmentServiceRouter.get("/:id", catchAsync(departmentServiceController.getServiceById));
+DepartmentServiceRouter.put("/:id", catchAsync(departmentServiceController.updateService));
+DepartmentServiceRouter.delete("/:id", catchAsync(departmentServiceController.deleteService));
+
+export default DepartmentServiceRouter;
