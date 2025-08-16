4960aa4fcdda2cd92a99c80bc64cdf0da4481530
Asela Priyadarshana <43272744+zaselalk@users.noreply.github.com>
2025-08-16 14:45:01 +0530

    :sparkles: create email sending utils (#136)
    * :sparkles:add email verification functionality for citizens
    
    * :bug: fix navigation path for login in email verification success page
    
    * :bug: simplify password validation and remove redundant hashing in citizen registration
    
    * :recycle: replace bcrypt with bcryptjs for password hashing consistency

--- api/src/routes/citizen.routes.ts
+++ api/src/routes/citizen.routes.ts
@@ -3,9 +3,12 @@ import catchAsync from "../util/catchAsync";
 import { CitizenController } from "../controllers/CitizenController";
 import {
   citizeLoginValidation,
+  citizenEmailVerificationValidation,
   citizenRegisterValidation,
 } from "../validation/citizen";
-import serializeCitizen, { requireCitizenAuth } from "../middleware/citizenAuth.middleware";
+import serializeCitizen, {
+  requireCitizenAuth,
+} from "../middleware/citizenAuth.middleware";
 
 const CitizenRouter: Router = Router();
 const citizenController = new CitizenController();
@@ -20,6 +23,15 @@ CitizenRouter.post(
   "/auth/login",
   citizeLoginValidation,
   catchAsync(citizenController.loginCitizen)
+);
+CitizenRouter.get(
+  "/auth/verify-email",
+  citizenEmailVerificationValidation,
+  catchAsync(citizenController.verifyEmail)
+);
+CitizenRouter.post(
+  "/auth/resend-verification",
+  catchAsync(citizenController.resendVerificationEmail)
 );
 CitizenRouter.get(
   "/auth/check",
