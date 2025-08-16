3b9a5fadf756755c0394ccd863d3072752a22391
RavinduHarshana <ravinduharshana2001@gmail.com>
2025-08-16 15:34:04 +0530

    :recycle: Refactor AppointmentDocument model by removing unused associations and simplifying initialization

--- api/src/models/appointmentDocument.ts
+++ api/src/models/appointmentDocument.ts
@@ -1,10 +1,5 @@
 import { Model, DataTypes } from "sequelize";
 import sequelize from "./sequelize";
-import { Appointment } from "./Appoinment";
-import { Citizen } from "./association";
-import { DocumentType } from "./association";
-
-
 
 export class AppointmentDocument extends Model {
   public id!: number;
@@ -14,91 +9,20 @@ export class AppointmentDocument extends Model {
   public file_path!: string;
   public mime_type!: string;
   public appointmentReferenceId!: string;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
 }
 
 AppointmentDocument.init(
   {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    citizen_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'citizens',
-        key: 'id',
-      },
-      onDelete: "CASCADE",
-      onUpdate: "CASCADE",
-    },
-    document_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'document_types',
-        key: 'doc_id',
-      },
-      onDelete: "CASCADE",
-      onUpdate: "CASCADE",
-    },
-    file_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    file_path: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    mime_type: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    appointmentReferenceId: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      references: {
-        model: 'appointment',
-        key: 'referenceId',
-      },
-      onDelete: "CASCADE",
-      onUpdate: "CASCADE",
-    },
+    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    citizen_id: { type: DataTypes.INTEGER, allowNull: false },
+    document_id: { type: DataTypes.INTEGER, allowNull: false },
+    file_name: { type: DataTypes.STRING, allowNull: false },
+    file_path: { type: DataTypes.STRING, allowNull: false },
+    mime_type: { type: DataTypes.STRING, allowNull: false },
+    appointmentReferenceId: { type: DataTypes.STRING, allowNull: false },
   },
-  {
-    sequelize,
-    tableName: "appointment_documents",
-  }
+  { sequelize, tableName: "appointment_documents" }
 );
-
-// Associations
-AppointmentDocument.belongsTo(Appointment, {
-  foreignKey: "appointmentReferenceId",
-  targetKey: "referenceId",
-});
-
-Appointment.hasMany(AppointmentDocument, {
-  foreignKey: "appointmentReferenceId",
-  sourceKey: "referenceId",
-});
-
-
-AppointmentDocument.belongsTo(Citizen, {
-  foreignKey: "citizen_id",
-  targetKey: "id",
-});
-
-Citizen.hasMany(AppointmentDocument, {
-  foreignKey: "citizen_id",
-  sourceKey: "id",
-});
-// Document Associations
-AppointmentDocument.belongsTo(DocumentType, {
-  foreignKey: "document_id",
-  targetKey: "doc_id",
-});
-DocumentType.hasMany(AppointmentDocument, {
-  foreignKey: "document_id",
-  sourceKey: "doc_id",
-});
