623a365f7acafc563058863ead81c9531635ef89
RavinduHarshana <ravinduharshana2001@gmail.com>
2025-08-16 15:56:19 +0530

    :sparkles: Implement AppointmentRepository for managing appointment data

---
+++ api/src/repositories/appointmentRepository.ts
@@ -0,0 +1,38 @@
+import { Appointment } from "../models/Appoinment";
+
+export class AppointmentRepository {
+    private static instance: AppointmentRepository;
+
+    private constructor() { }
+
+    public static getInstance(): AppointmentRepository {
+        if (!AppointmentRepository.instance) {
+            AppointmentRepository.instance = new AppointmentRepository();
+        }
+        return AppointmentRepository.instance;
+    }
+
+    public async createAppointment(data: Partial<Appointment>) {
+        return Appointment.create(data);
+    }
+
+    public async findAllAppointments(): Promise<Appointment[]> {
+        return Appointment.findAll();
+    }
+
+    public async findAppointmentById(id: string): Promise<Appointment | null> {
+        return Appointment.findByPk(id);
+    }
+
+    public async updateAppointment(id: string, data: Partial<Appointment>): Promise<[number]> {
+        return Appointment.update(data, { where: { id } });
+    }
+
+    public async deleteAppointment(id: string): Promise<number> {
+        const appointment = await this.findAppointmentById(id);
+        if (!appointment) {
+            throw new Error("Appointment not found");
+        }
+        return Appointment.destroy({ where: { id } });
+    }
+}
