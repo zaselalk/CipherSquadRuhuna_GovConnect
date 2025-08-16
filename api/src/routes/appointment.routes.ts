60153418ad3091eec512ac45d46abffa3c4b1843
RavinduHarshana <ravinduharshana2001@gmail.com>
2025-08-16 15:55:17 +0530

    :sparkles: Add appointment routes for CRUD operations

---
+++ api/src/routes/appointment.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { AppointmentController } from "../controllers/appointmentController";
+import { AppointmentService } from "../services/appointmentService";
+import catchAsync from "../util/catchAsync";
+
+const AppointmentRoutes = Router();
+
+const appointmentController = new AppointmentController();
+
+AppointmentRoutes.get("/", catchAsync(appointmentController.getAll));
+AppointmentRoutes.get("/:id", catchAsync(appointmentController.getById));
+AppointmentRoutes.post("/", catchAsync(appointmentController.create));
+AppointmentRoutes.put("/:id", catchAsync(appointmentController.update));
+AppointmentRoutes.delete("/:id", catchAsync(appointmentController.delete));
+
+export default AppointmentRoutes;
