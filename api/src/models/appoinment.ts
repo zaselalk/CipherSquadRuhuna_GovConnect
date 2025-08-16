dfc92dec7c06b5ddc705e4b97261aaf8a49a0909
RavinduHarshana <ravinduharshana2001@gmail.com>
2025-08-16 15:55:43 +0530

    :recycle: Refactor Appointment model by removing unused associations and simplifying initialization

--- api/src/models/appoinment.ts
+++ api/src/models/appoinment.ts
@@ -1,6 +1,5 @@
 import { Model, DataTypes } from "sequelize";
 import sequelize from "./sequelize";
-import { AppointmentDocument } from "./appointmentDocument";
 
 export class Appointment extends Model {
   public appointmentId!: number;
@@ -9,69 +8,19 @@ export class Appointment extends Model {
   public appointmentDate!: string;
   public appointmentTime!: string;
   public referenceId!: string;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
 }
 
 Appointment.init(
   {
-    appointmentId: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    citizenId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'citizen',
-        key: 'id',
-      },
-      onDelete: "CASCADE",
-      onUpdate: "CASCADE",
-    },
-    serviceId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'department-service',
-        key: 'service_id',
-      },
-      onDelete: "CASCADE",
-      onUpdate: "CASCADE",
-    },
-    appointmentDate: {
-      type: DataTypes.DATEONLY,
-      allowNull: false,
-    },
-    appointmentTime: {
-      type: DataTypes.TIME,
-      allowNull: false,
-    },
-    referenceId: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      references: {
-        model: 'appointment',
-        key: 'referenceId',
-      },
-    },
-
+    appointmentId: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    citizenId: { type: DataTypes.INTEGER, allowNull: false },
+    serviceId: { type: DataTypes.INTEGER, allowNull: false },
+    appointmentDate: { type: DataTypes.DATEONLY, allowNull: false },
+    appointmentTime: { type: DataTypes.TIME, allowNull: false },
+    referenceId: { type: DataTypes.STRING, allowNull: false, unique: true },
   },
-  {
-    sequelize,
-    tableName: "appointments",
-  }
+  { sequelize, tableName: "appointments" }
 );
-
-// Associations
-Appointment.hasMany(AppointmentDocument, {
-  foreignKey: "appointmentReferenceId",
-  sourceKey: "referenceId",
-});
-
-AppointmentDocument.belongsTo(Appointment, {
-  foreignKey: "appointmentReferenceId",
-  targetKey: "referenceId",
-});
-
-
